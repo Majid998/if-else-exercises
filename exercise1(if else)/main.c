86b7648e1582ba4db534e9cd23012acf12f51e99
Majid Mujahid Hussain <47850376+Majid998@users.noreply.github.com>
2020-12-18 09:59:09 +0530

    Add files via upload

---
+++ exercise1(if else)/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+// int a = 300, b, c ;
+// if ( a >= 400 )
+// b = 300 ;
+// c = 200 ;
+// printf ( "\n%d %d", b, c ) ;
+//
+//
+// int x = 10, y = 20 ;
+// if ( x == y ) ;
+// printf ( "\n%d %d", x, y ) ;
+
+//   int x = 3, y = 5 ;
+// if ( x == 3 )
+// printf ( "\n%d", x ) ;
+// else ;
+// printf ( "\n%d", y ) ;
+
+
+
+// int k = 35 ;
+// printf ( "\n%d %d %d", k == 35, k = 50, k > 40 ) ;
+//
+
+//int i = 65 ;
+//char j = 'A';
+// if ( i == j )
+// printf ( "C is WOW" ) ;
+// else
+// printf( "C is a headache" ) ;
+//
+
+
+// int a = 5, b, c ;
+// b = a = 15 ;
+// c = a < 15 ;
+// printf ( "\na = %d b = %d c = %d", a, b, c ) ;
+//
+
+// int j = 10, k = 12 ;
+// if ( k >= j )
+// {//this to be removed
+//  {
+//   k = j ;
+//   j = k ;
+//  }
+// }//this to be removed
+
+
+// if ( 'X' < 'x' ) no problem with the program.
+// printf ( "\nascii value of X is smaller than that of x" ) ;
+
+//int x = 10, y = 15 ;
+// if ( x % 2 = y % 3 )//need to use == instead of =
+//   { printf ( "\nCarpathians" ) ;}
+
+//int x = 30 , y = 40 ;
+// if ( x == y )
+// printf( "x is equal to y" ) ;
+// elseif ( x > y )//here elseif is to be written else if
+// printf( "x is greater than y" ) ;
+// elseif ( x < y )//here elseif is to be written else if
+// printf( "x is less than y" ) ;
+//
+// int a, b ;
+// scanf ( "%d %d",a, b ) ;// to be written as scanf("%d %d, a,&b) ;
+// if ( a > b ); // no need to add ; after if condition.
+// printf ( "This is a game" ) ;
+// else
+// printf ( "You have to play it" ) ;
+
+    return 0;
+}
